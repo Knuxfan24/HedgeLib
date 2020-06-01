72f170184f298a09a76455a986588afc1ef13188
HyperPolygon64 <doctorherobrine6@gmail.com>
2020-06-01 04:11:35 +0100

    Very preliminary SegaNN stuff 	modified:   HedgeLib.sln 	modified:   HedgeLib/HedgeLib.csproj 	new file:   HedgeLib/Models/S06XnFile.h 	new file:   HedgeLib/Models/SegaNNObject.cs 	new file:   HedgeTest/App.config 	new file:   HedgeTest/HedgeTest.csproj 	new file:   HedgeTest/Program.cs 	new file:   HedgeTest/Properties/AssemblyInfo.cs

---
+++ HedgeLib/Models/SegaNNObject.cs
@@ -0,0 +1,91 @@
+﻿using HedgeLib.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HedgeLib.Models
+{
+    public class NXIF
+    {
+        public string HeaderInfoNode;
+        public uint NodeLength;
+        public uint NodeCount;
+    }
+
+    public class NXTL
+    {
+        public string TextureListNode;
+        public uint NodeLength;
+        public uint TextureCount;
+
+        public List<string> Textures = new List<string>();
+    }
+
+    public class NXEF
+    {
+        public string EffectNode;
+        public uint NodeLength;
+        public uint EffectCount;
+
+        public List<string> Effects = new List<string>();
+    }
+
+    public class SegaNNObject : FileBase
+    {
+        public override void Load(Stream fileStream)
+        {
+            ExtendedBinaryReader reader = new ExtendedBinaryReader(fileStream) { Offset = 0x20 };
+
+            // NINJA XBOX INFO [NXIF]
+            NXIF infoList = new NXIF()
+            {
+                HeaderInfoNode = new string(reader.ReadChars(4)),
+                NodeLength = reader.ReadUInt32(),
+                NodeCount = reader.ReadUInt32()
+            };
+
+            reader.JumpTo(infoList.NodeLength + 8);
+
+            // NINJA XBOX TEXTURE LIST [NXTL]
+            NXTL textureList = new NXTL()
+            {
+                TextureListNode = new string(reader.ReadChars(4)),
+                NodeLength = reader.ReadUInt32()
+            };
+
+            reader.JumpTo(reader.ReadUInt32(), false);
+            textureList.TextureCount = reader.ReadUInt32();
+            uint textureListOffset = reader.ReadUInt32();
+            reader.JumpTo(textureListOffset, false);
+
+            for (int i = 0; i < textureList.TextureCount; i++)
+            {
+                reader.JumpAhead(0x4);
+                uint textureNameOffset = reader.ReadUInt32();
+                long pos = reader.BaseStream.Position; //Save Position
+                reader.JumpTo(textureNameOffset, false);
+                textureList.Textures.Add(reader.ReadNullTerminatedString());
+                reader.JumpTo(pos);
+                reader.JumpAhead(0xC);
+            }
+
+            // NINJA XBOX EFFECTS [NXEF]
+            NXEF effectList = new NXEF()
+            {
+                EffectNode = new string(reader.ReadChars(4)),
+                NodeLength = reader.ReadUInt32()
+            };
+
+
+
+            // NINJA XBOX NODE NAMES [NXNN]
+
+            // NINJA XBOX OBJECTS [NXOB]
+
+            // NINJA XBOX MATERIALS [NXMT]
+        }
+    }
+}
---
+++ HedgeTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HedgeLib.Models;
+
+namespace HedgeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SegaNNObject xno = new SegaNNObject();
+            xno.Load(@"C:\Users\gabe1\AppData\Local\Hyper_Development_Team\Sonic '06 Toolkit\Archives\78988\1z2ovl5b.2zp\player_sonic\win32\player\sonic_new\so_itm_sbungle_L.xno");
+        }
+    }
+}
